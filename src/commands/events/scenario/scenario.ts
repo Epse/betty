f3d2649a7453d1fe6aa736eefa1ed5775b6e5da4
Stef Pletinck <s@stefpletinck.be>
2024-06-24 07:57:47 +0200

    Switch scenario to subcommands
    This allows us to support custom intensity,
    and reduces the amount of chat interaction required.
    
    That second point also happens to make the dev experience much,
    much nicer. #winning

--- src/commands/events/scenario/scenario.ts
+++ src/commands/events/scenario/scenario.ts
@@ -13,19 +13,14 @@ This gets fed into the script.
 import {Command} from "../../../types/command";
 import {PublicAuthorization} from "../../../authorization/authorize";
 import {
-    ActionRowBuilder, BaseInteraction,
-    ChatInputCommandInteraction, Interaction,
-    MessageActionRowComponentBuilder, MessageComponentInteraction,
+    ChatInputCommandInteraction,
     SlashCommandBuilder,
-    StringSelectMenuBuilder, StringSelectMenuInteraction,
-    StringSelectMenuOptionBuilder
 } from "discord.js";
-import {interactionIdSuffix} from "../../../util/interaction";
 import {getVfrIntensity} from "./vfr_intensity";
 import {getRunwayConfig} from "./runway_config";
+import {airportOption, pseudoPilotOption} from "./common_options";
 
 // TODO: handle timeouts
-const supported_icao = ['EBBR', 'EBAW', 'EBOS', 'EBCI', 'EBLG', 'ELLX'];
 
 // TODO is authz necessary?
 export default {
@@ -34,62 +29,92 @@ export default {
         .setName('scenario')
         .setDMPermission(true)
         .setDescription('Build scenario file for training.')
-        .addStringOption(option =>
-            option
-                .setName('airport')
-                .setDescription('ICAO code for the airport')
-                .setRequired(true)
-                .setMinLength(4)
-                .setMaxLength(4)
-                .setChoices(supported_icao.map(x => ({name: x, value: x})))
+        .addSubcommand(command =>
+            command
+                .setName('low')
+                .setDescription('Low intensity scenario')
+                .addStringOption(option => airportOption(option))
+                .addStringOption(option => pseudoPilotOption(option))
         )
-        .addStringOption(option =>
-            option
-                .setName('intensity')
-                .setDescription('Intensity')
-                .setRequired(true)
-                .setChoices(
-                    ['Low', 'Medium', 'High', 'VFR', 'Custom'].map(x => ({name: x, value: x}))
+        .addSubcommand(command =>
+            command
+                .setName('medium')
+                .setDescription('Medium intensity scenario')
+                .addStringOption(option => airportOption(option))
+                .addStringOption(option => pseudoPilotOption(option))
+        )
+        .addSubcommand(command =>
+            command
+                .setName('high')
+                .setDescription('High intensity scenario')
+                .addStringOption(option => airportOption(option))
+                .addStringOption(option => pseudoPilotOption(option))
+        )
+        .addSubcommand(command =>
+            command
+                .setName('vfr')
+                .setDescription('VFR scenario')
+                .addStringOption(option => airportOption(option))
+                .addStringOption(option => pseudoPilotOption(option))
+                .addIntegerOption(option =>
+                    option.setName('vfr_dep')
+                        .setDescription('Amount of VFR Departures')
+                        .setMinValue(0)
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('vfr_arr')
+                        .setDescription('Amount of VFR Arrivals')
+                        .setMinValue(0)
+                        .setRequired(true)
                 )
         )
-        .addStringOption(option =>
-            option
-                .setName('pseudo_pilot')
-                .setDescription('Pseudo Pilot station')
-                .setRequired(true)
-                .setChoices(
-                    ["EBBR_TWR", "EBBR_APP", "ELLX_APP", "EBLG_APP", "EBOS_APP", "EBBU_CTR"]
-                        .map(x => ({name: x, value: x}))
+        .addSubcommand(command =>
+            command
+                .setName('custom')
+                .setDescription('Custom scenario')
+                .addStringOption(option => airportOption(option))
+                .addStringOption(option => pseudoPilotOption(option))
+                .addIntegerOption(option =>
+                    option.setName('start_spawns')
+                        .setDescription('Start spawns')
+                        .setMinValue(0)
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('ifr_dep')
+                        .setDescription('Amount of IFR Departures')
+                        .setMinValue(0)
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('ifr_arr')
+                        .setDescription('Amount of IFR Arrivals')
+                        .setMinValue(0)
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('vfr_dep')
+                        .setDescription('Amount of VFR Departures')
+                        .setMinValue(0)
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('vfr_arr')
+                        .setDescription('Amount of VFR Arrivals')
+                        .setMinValue(0)
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('faults')
+                        .setDescription('Faults')
+                        .setMinValue(0)
+                        .setRequired(true)
                 )
         )
     ,
     async execute(interaction: ChatInputCommandInteraction): Promise<void> {
         await interaction.reply({
-            ephemeral: true,
-            content: "Okay, I have some questions for you!"
-        });
-
-        const runwayConfig = await getRunwayConfig(interaction.options.getString('airport'), interaction);
-        await interaction.followUp({
-            ephemeral: true,
-            content: `
-We got:
-- Airport: ${interaction.options.getString('airport')}
-- Runway config: ${runwayConfig}
-- Intensity: ${interaction.options.getString('intensity')}
-- Pseudo-pilot: ${interaction.options.getString('pseudo_pilot')}
-            `
-        });
-
-        if (interaction.options.getString('intensity') === 'VFR') {
-            const vfrIntensity = await getVfrIntensity(interaction);
-            await interaction.followUp({
-                ephemeral: true,
-                content: `We also got VFR departures: ${vfrIntensity.departures} and arrivals: ${vfrIntensity.arrivals}`
-            });
-        }
-
-        await interaction.followUp({
             ephemeral: true,
             content: "👷‍♀️ We're still building this! 🚧"
         });
